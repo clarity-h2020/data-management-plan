b450d778d47ad9abe2d7856cf07b46ab6466f8c3
p-a-s-c-a-l <pascal.dihe@cismet.de>
2020-03-26 07:51:23 +0100

    #1 bootstrap node project

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const carbone = require('carbone');
+
+  // Data to inject
+  var data = {
+    firstname : 'John',
+    lastname : 'Doe'
+  };
+
+  // Generate a report using the sample template provided by carbone module
+  // This LibreOffice template contains "Hello {d.firstname} {d.lastname} !"
+  // Of course, you can create your own templates!
+  carbone.render('./node_modules/carbone/examples/simple.odt', data, function(err, result){
+	console.log('Generate a report using the sample template provided by carbone module');
+    if (err) {
+	  console.log(err);
+      return console.log(err);
+    } else {
+		console.log('Report successfully generated using the sample template provided by carbone module');
+	}
+    // write the result
+    fs.writeFileSync('output/result.odt', result);
+  });
