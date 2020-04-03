0a7fcdb1c6480ac9635385ab3babb473ea4350fd
p-a-s-c-a-l <pascal.dihe@cismet.de>
2020-04-03 13:27:05 +0200

    #4 get max 1000 datasets

--- input/groups.sh
+++ input/groups.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd "${0%/*}"
-ckanapi action group_package_show id='non-open-data-produced-by-clarity' -r https://ckan.myclimateservice.eu/  > non-open-data-produced-by-clarity.json
-ckanapi action group_package_show id='non-open-data-used-by-clarity' -r https://ckan.myclimateservice.eu/  > non-open-data-used-by-clarity.json
-ckanapi action group_package_show id='open-data-produced-by-clarity' -r https://ckan.myclimateservice.eu/  > open-data-produced-by-clarity.json
-ckanapi action group_package_show id='open-data-used-by-clarity' -r https://ckan.myclimateservice.eu/  > open-data-used-by-clarity.json
+ckanapi action group_package_show id='non-open-data-produced-by-clarity' limit=1000 -r https://ckan.myclimateservice.eu/  > non-open-data-produced-by-clarity.json
+ckanapi action group_package_show id='non-open-data-used-by-clarity' limit=1000 -r https://ckan.myclimateservice.eu/  > non-open-data-used-by-clarity.json
+ckanapi action group_package_show id='open-data-produced-by-clarity' limit=1000 -r https://ckan.myclimateservice.eu/  > open-data-produced-by-clarity.json
+ckanapi action group_package_show id='open-data-used-by-clarity' limit=1000 -r https://ckan.myclimateservice.eu/  > open-data-used-by-clarity.json
--- input/organizations.sh
+++ input/organizations.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 cd "${0%/*}"
-ckanapi action package_search fq=organization:clarity -r https://ckan.myclimateservice.eu/ > clarity.json
-ckanapi action package_search fq=organization:dc1 -r https://ckan.myclimateservice.eu/ > dc1.json
-ckanapi action package_search fq=organization:dc2 -r https://ckan.myclimateservice.eu/ > dc2.json
-ckanapi action package_search fq=organization:dc3 -r https://ckan.myclimateservice.eu/ > dc3.json
-ckanapi action package_search fq=organization:dc4 -r https://ckan.myclimateservice.eu/ > dc4.json
+ckanapi action package_search fq=organization:clarity rows=1000 -r https://ckan.myclimateservice.eu/ > clarity.json
+ckanapi action package_search fq=organization:dc1 rows=1000 -r https://ckan.myclimateservice.eu/ > dc1.json
+ckanapi action package_search fq=organization:dc2 rows=1000 -r https://ckan.myclimateservice.eu/ > dc2.json
+ckanapi action package_search fq=organization:dc3 rows=1000 -r https://ckan.myclimateservice.eu/ > dc3.json
+ckanapi action package_search fq=organization:dc4 rows=1000 -r https://ckan.myclimateservice.eu/ > dc4.json
